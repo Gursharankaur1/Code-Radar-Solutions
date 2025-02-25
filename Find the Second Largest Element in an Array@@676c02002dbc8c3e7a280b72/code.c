46e93d7b2023e764bde1d9c851a0d2a268f26e80
Gursharankaur1 <gursharan3797@gmail.com>
2025-02-25 14:30:19 +0530

    Solution to Find the Second Largest Element in an Array

--- Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
+++ Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
@@ -1,30 +1,36 @@
 #include <stdio.h>
 #include <limits.h>
-int main(){
+
+int main() {
     int n;
-    scanf("%d",&n);
+    scanf("%d", &n);
+
+    if (n < 2) {  // If there is only one element, return -1 immediately
+        printf("-1\n");
+        return 0;
+    }
+
     int array[n];
-    for(int i=0;i<n;i++){
-        scanf("%d",&array[i]);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &array[i]);
     }
-    int max1=INT_MIN, max2=INT_MIN;
-    for(int j=0;j<n;j++){
-        if(array[j]>max1){
-            max2=max1;
-            max1=array[j];
+
+    int max1 = INT_MIN, max2 = INT_MIN;
+
+    for (int j = 0; j < n; j++) {
+        if (array[j] > max1) {
+            max2 = max1;
+            max1 = array[j];
+        } else if (array[j] < max1 && array[j] > max2) {  // Ensure distinct values
+            max2 = array[j];
         }
-        else if(array[j]>max2){
-            max2=array[j];
-        }
     }
-    if(max1==max2){
-        printf("%d",-1);
+
+    if (max2 == INT_MIN) {  // If no second largest number was found
+        printf("-1\n");
+    } else {
+        printf("%d\n", max2);
     }
-    else if(n==1){
-        printf("%d",-1);
-    }
-    else{
-        printf("%d",max2);
-    }
+
     return 0;
 }
